17cf99770aaa003d723ce92a40a18088b2df44e2
valezim <valezimberg@gmail.com>
2021-09-20 16:30:43 -0300

    3 files modified

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  registerGroup;
+  errMsg: any;
+    constructor(   
+       private formBuilder: FormBuilder,
+       private userService: UserService,
+       private router: Router
+       ) { 
+  
+      this.registerGroup = this.formBuilder.group({
+        usuario: '',
+        password: ''
+      });
+  
+    }
+  
+  
+    ngOnInit() {
+    }
+  
+    formSubmit() {
+      this.errMsg = '';
+      const { usuario, password } = this.registerGroup.value;
+      this.userService.register(usuario, password).subscribe(
+        user => {
+          this.userService.setUser(user, usuario);
+          this.router.navigate(['/dashboard']);
+        },
+        ({ error: { mensaje } }) => {
+          this.errMsg = mensaje;
+        }
+      );
+    }
+  
 }
