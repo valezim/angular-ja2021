fb9b0faffa9df092b2d11f2297a14415c1e2c27f
valezim <valezimberg@gmail.com>
2021-09-22 20:27:07 -0300

    5 files modified

--- src/app/components/cantidad-paquetes/cantidad-paquetes.component.ts
+++ src/app/components/cantidad-paquetes/cantidad-paquetes.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, Output } from '@angular/core';
+import { VentaService } from '../../services/venta.service';
 
 @Component({
   selector: 'app-cantidad-paquetes',
   templateUrl: './cantidad-paquetes.component.html',
-  styleUrls: ['./cantidad-paquetes.component.css']
+  styleUrls: ['./cantidad-paquetes.component.css'],
 })
 export class CantidadPaquetesComponent implements OnInit {
+  constructor(private ventaService: VentaService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  getCantidadPaquetesVendidos() {
+    return this.ventaService?.getPaquetes()?.length;
   }
-
 }
