3375ce6ddc08414bfa8af4deb19ba139f1094dc9
valezim <valezimberg@gmail.com>
2021-09-23 20:11:30 -0300

    2 files modified, 1 added

--- src/app/components/destinos-sin-ventas/destinos-sin-ventas.component.ts
+++ src/app/components/destinos-sin-ventas/destinos-sin-ventas.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-destinos-sin-ventas',
   templateUrl: './destinos-sin-ventas.component.html',
-  styleUrls: ['./destinos-sin-ventas.component.css']
+  styleUrls: ['./destinos-sin-ventas.component.css'],
 })
 export class DestinosSinVentasComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
